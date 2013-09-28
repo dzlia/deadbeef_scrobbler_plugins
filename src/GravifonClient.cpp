2a7a2051c667cba3812ebfe2566f08661ffde8c0
Dźmitry Laŭčuk <>
2013-09-28 23:27:41 +0300

    A dummy implementation of GravifonClient is added.

---
+++ src/GravifonClient.cpp
@@ -0,0 +1,41 @@
+/* gravifon_scrobbler - an audio track scrobbler to Gravifon plugin to the audio player DeaDBeeF.
+Copyright (C) 2013 Dźmitry Laŭčuk
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+#include "GravifonClient.hpp"
+#include <stdexcept>
+
+using std::invalid_argument;
+
+GravifonClient::GravifonClient(const char *scrobblerUrl, const char *username, const char *password)
+{
+	if (scrobblerUrl == nullptr) {
+		throw invalid_argument("scrobblerUrl");
+	}
+	if (username == nullptr) {
+		throw invalid_argument("username");
+	}
+	if (password == nullptr) {
+		throw invalid_argument("password");
+	}
+
+	m_scrobblerUrl = scrobblerUrl;
+	m_username = username;
+	m_username = password;
+}
+
+void GravifonClient::scrobble(const ScrobbleInfo &scrobbleInfo)
+{
+	// TODO implement me
+}
---
+++ src/GravifonClient.hpp
@@ -0,0 +1,59 @@
+/* gravifon_scrobbler - an audio track scrobbler to Gravifon plugin to the audio player DeaDBeeF.
+Copyright (C) 2013 Dźmitry Laŭčuk
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+#ifndef GRAVIFONCLIENT_HPP_
+#define GRAVIFONCLIENT_HPP_
+
+#include <string>
+
+// All strings are utf8-encoded.
+struct Track
+{
+	std::string track;
+	std::string artist;
+	// TODO think about supporting full album info
+	std::string album;
+	long duration;
+	long trackNumber;
+};
+
+struct ScrobbleInfo
+{
+	// Date and time when scrobble event was initiated.
+	long scrobbleStartTimestamp;
+	// Date and time when scrobble event was finished.
+	long scrobbleEndTimestamp;
+	// Scrobble length.
+	long scrobbleDuration;
+	// Track to scrobble.
+	Track track;
+};
+
+class GravifonClient
+{
+public:
+	// TODO declare exceptions
+	GravifonClient(const char *scrobblerUrl, const char *username, const char *password);
+	~GravifonClient() {};
+
+	// TODO declare exceptions
+	void scrobble(const ScrobbleInfo &);
+private:
+	std::string m_scrobblerUrl;
+	std::string m_username;
+	std::string m_password;
+};
+
+#endif /* GRAVIFONCLIENT_HPP_ */
