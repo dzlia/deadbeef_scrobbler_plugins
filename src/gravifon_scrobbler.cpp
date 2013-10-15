db70ff313589f2a60fedb88815b69bd9b1af6f6c
Dźmitry Laŭčuk <>
2013-10-16 02:22:14 +0300

    A debug header is removed.

--- src/gravifon_scrobbler.cpp
+++ src/gravifon_scrobbler.cpp
@@ -20,7 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 #include <chrono>
 #include <mutex>
 #include <cstring>
-#include <iostream>
 
 using namespace std;
 using std::chrono::system_clock;
